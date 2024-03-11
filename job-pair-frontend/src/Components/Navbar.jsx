8464ae27a950650166de690381cefee41cb0588d
Abhay Sharma <sabhay2001@gmail.com>
2024-03-11 02:04:55 -0600

    adding Navbar component

--- job-pair-frontend/src/Components/Navbar.jsx
+++ job-pair-frontend/src/Components/Navbar.jsx
@@ -1,27 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Styles/Navbar.css'; // Make sure to create a corresponding CSS file
 import logo from '../Assets/Job-pair-small 1.png'; // Import the image here
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <img src={logo} alt='Brand logo' />
-      <div className="menu">
-        <a href="/jobs">Jobs</a>
-        <a href="/interviews">Interviews</a>
-        <a href="/tracking">Tracking</a>
-        <a href="/profile" className="active">Profile</a>
-        <a href="/chat">Chat</a>
-        <div className="search-bar">
-          <input type="text" placeholder="Search" />
+    const [active, setActive] = useState('jobs'); // Default active is 'jobs'
+
+    const handleToggle = (buttonId) => {
+        setActive(buttonId);
+    };
+
+    const buttons = [
+        { id: 'jobs', text: 'Jobs' },
+        { id: 'interviews', text: 'Interviews' },
+        { id: 'tracking', text: 'Tracking' },
+        { id: 'profile', text: 'Profile' },
+        { id: 'chat', text: 'Chat' }
+    ];
+    return (
+        <nav className="navbar">
+        <img src={logo} alt='Brand logo' />
+        <div className="menu">
+            <div className='menu-options'>
+            {buttons.map((button) => (
+                <a
+                key={button.id}
+                href={'/' + button.id}
+                className={`toggle-button ${active === button.id ? 'active' : ''}`}
+                onClick={(e) => {
+                    e.preventDefault(); // Prevent default anchor link behavior
+                    handleToggle(button.id);
+                }}
+                >
+                {button.text}
+                </a>
+            ))}
+            </div>
+            <div className="search-bar">
+                <input type="text" placeholder="Search" />
+            </div>
+            <a className="logout-button" href="/logout">Logout</a>
         </div>
-        <a href="/logout">Logout</a>
-      </div>
-      <div className="menu-icon">
-        {/* Icon to show/hide the menu on small screens */}
-      </div>
-    </nav>
-  );
+        
+        <div className="menu-icon">
+            {/* Icon to show/hide the menu on small screens */}
+        </div>
+        </nav>
+    );
 };
 
 export default Navbar;
