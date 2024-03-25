79f4439564037830092046e88404b11a4d51a9c2
Abhay Sharma <sabhay2001@gmail.com>
2024-03-25 03:35:43 -0600

    containerizing the backend and adding a dummy server for now

---
+++ job-pair-backend/test.py
@@ -0,0 +1,13 @@
+import requests
+
+# URL of the Flask app
+url = "http://localhost:5000/"
+
+# Send a GET request
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    print("Response from the server:", response.text)
+else:
+    print("Failed to get a response from the server", response.status_code)
