cd8ae5f213ca6dad04c903ec3e822658366326f4
Abhay Sharma <sabhay2001@gmail.com>
2024-03-25 05:34:48 -0600

    dockerising the frontend

--- job-pair-frontend/src/App.js
+++ job-pair-frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Route path="/userprofile/:id" element={<Profilepage/>} />
       <Route path="/companyprofile/:id" element={<Companyprofilepage/>} />
       <Route path="/jobpage/:id" element={<Jobpage/>} />
-      <Route path="/applicants/:id" element={<JobApplicants/>} />
+      <Route path="/applicants/:id" element={<JobApplicants/>} /> 
       {/* <Route path="/tracking" element={<TrackingPage2 />} />
       <Route path="/scholarships" element={<JobsPage />} />
       <Route path="/salaries" element={<SalariesPage />} />
